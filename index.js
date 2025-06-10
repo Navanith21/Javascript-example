72b650947bfa76deb4cf5dd6be05dad7268e9faf
Navanith21 <navanithsenthil2006@gmail.com>
2025-06-10 16:15:28 +0530

    scoreboard

---
+++ index.js
@@ -0,0 +1,17 @@
+function increment1(){
+    team1Score += 1
+    console.log(increment1)
+}
+function increment2(){
+    team1Score += 2
+    console.log(increment2)
+}
+function increment3(){
+    team1Score += 3
+    console.log(increment3)
+}
+
+function reset(){
+    team1Score =0
+    console.log(team1Score)
+}
